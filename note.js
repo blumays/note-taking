965aeda770aa3b0a8aad972465b0d0eb9e41226e
Blu Mays <bmays@dcpmidstream.com>
2019-09-28 09:57:19 -0600

    push

---
+++ note.js
@@ -0,0 +1,13 @@
+const onFormSubmit = () => {
+    const formData = readFormData();
+}
+
+const readFormData = () => {
+    const formData = {};
+    formData["text"] = document.getElementById("text").nodeValue;
+
+}
+
+const insertNewRecods = (data) => {
+    
+}
